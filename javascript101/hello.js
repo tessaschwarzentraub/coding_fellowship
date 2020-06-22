b77270ec56d5ba873b470b1fc0f7be0ad5ee8821
Tessa Schwarzentraub <tlschwarzentraub@gmail.com>
2020-06-22 09:57:14 -0500

    Calculator design/ rebuilt group calculator
    added CSS to my first (personal) calculator design. deleted the group calculator and tried to rebuild- no css, the output is coming up as undefined

--- javascript101/hello.js
+++ javascript101/hello.js
@@ -1,6 +1,3 @@
-// when the submit button is clicked do math
-// when x math operator is selected do x type of math and then print the result
-
 function doMath(){
     let getNum1 = document.getElementById("num1").value;
     let getNum2 = document.getElementById("num2").value;
@@ -11,11 +8,11 @@ function doMath(){
         result = Number(getNum1) + Number(getNum2);
     }else if(getMathOperator == "subtract"){
         result = Number(getNum1) - Number(getNum2);
-    }else if(getMathOperator == "multiply"){
-        result = Number(getNum1) * Number(getNum2);
     }else if(getMathOperator == "divide"){
         result = Number(getNum1) / Number(getNum2);
+    }else if(getMathOperator == "multiply") {
+        result = Number(getNum1) * Number(getNum2);
     }
-    
-    document.getElementById("printResult").innerHTML = result;
+
+    document.getElementById("printResults").innerHTML = result;
 }
