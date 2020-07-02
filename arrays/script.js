e1677c66c74161806219f24aaf5bd4d495bf8b6a
Tessa Schwarzentraub <tlschwarzentraub@gmail.com>
2020-07-02 11:47:53 -0500

    objects and arrays
    live coding hour
    
    learned the basics of objects and arrays

---
+++ arrays/script.js
@@ -0,0 +1,14 @@
+let groceries = [                           //"let groceries is the variable", the [] is the array
+    "avocados", 
+    "coconut milk", 
+    "lentils",                              //eat different idem is a "memory space"
+    "onion", 
+    "tomatoes"
+];                                          //can also list memory spaces vertically not horizontally, just depends on writer
+
+console.log(groceries[0]);
+
+groceries.push("kiwis");                    //push puts a new item at the end of the list in an array  
+
+console.log(groceries);                     //regular console.log() shows the entire array
+
