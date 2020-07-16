86c5bc83b4375029d41a96781642f17664feca59
Tessa Schwarzentraub <tlschwarzentraub@gmail.com>
2020-07-16 11:26:58 -0500

    weather app changes
    corrected API
    called city name, current conditions and temperature to appear in browser

--- WeatherApp/app.js
+++ WeatherApp/app.js
@@ -1,13 +1,18 @@
-window.addEventListener('load', ()=> {
+window.addEventListener('load', ()=> {                  //get the location after the page loads
     let long;
     let lat;
+    let temperatureDescripton = document.querySelector(".temperature-description");
+    let temperatureDegree = document.querySelector(".temperature-degree");
+    let locationTimezone = document.querySelector(".location-timezone"); 
+    let skycons = document.querySelector(".icon");   
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
             long = position.coords.longitude;
             lat = position.coords.latitude;
 
-            const api = 'https://api.openweathermap.org/data/2.5/weather?+4905687&appid=+b27e22b55ef172af0896b15f864f7d26';
+            const proxy = "https://cors-anywhere.herokuapp.com/";
+            const api = `${proxy}https://api.openweathermap.org/data/2.5/weather?id=4905687&appid=b27e22b55ef172af0896b15f864f7d26`;
            
             fetch(api)
                 .then(response => { 
@@ -15,7 +20,19 @@ window.addEventListener('load', ()=> {
                 })
                 .then(data => {
                     console.log(data);
+                    const { temp } = data.main;
+                    const { description } = data.weather[0];
+                    //set DOM elements from the API
+                    temperatureDegree.textContent = temp;
+                    temperatureDescripton.textContent = description;
+                    locationTimezone.textContent = data.name;
                 });
         });
-    }  
+    
+    let skycons = new Skycons ({"color" : "white"});
+    skycons.add("icon1", Skycons.PARTLY_CLOUDY_DAY);
+    skycons.play();
+
+    }
+    
 });
