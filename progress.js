4df5b08fddc80e29b7608efd3ec1c2429cc13424
Tessa Schwarzentraub <tlschwarzentraub@gmail.com>
2020-07-29 13:42:02 -0500

    progress page
    added an image slider for my projects throughout the summer
    
    prev/nav buttons still need to be styled but slider is fully functional
    
    added links on all pages to the progress page

---
+++ progress.js
@@ -0,0 +1,44 @@
+const slide = document.querySelector('.slide');
+const images = document.querySelectorAll('.slide img');
+
+//buttons
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
+
+
+//counter
+let counter = 1;
+const size = images[0].clientWidth;
+
+slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+//button listeners 
+
+nextBtn.addEventListener('click',()=>{
+    if(counter >= images.length -1) return;
+    slide.style.transition = "transform 0.4s ease-in-out";
+    counter++;
+    slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+});
+
+prevBtn.addEventListener('click',()=>{
+    if (counter <= 0) return;
+    slide.style.transition = "transform 0.4s ease-in-out";
+    counter--;
+    slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+});
+
+slide.addEventListener('transitionend', ()=>{
+    if(images[counter].id === 'lastClone'){
+        slide.style.transition = "none";
+        counter = images.length -2;
+        slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
+    if(images[counter].id === 'firstClone'){
+        slide.style.transition = "none";
+        counter = images.length - counter;
+        slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
+});
